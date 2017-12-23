31bb12c5541d8e0eaeb0515aee71e0cd6fe2449d
Felix Bernhardt <felixbernhardt@yahoo.com>
2017-12-23 11:06:59 +0100

    update required python version

--- setup.py
+++ setup.py
@@ -70,6 +70,6 @@ setup(
             'ptag=prototag.cli:main',
         ],
     },
-    python_requires='>=3',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !3.3.*, <4',
     cmdclass={'test': RunTests},
 )
